b593ad64d0a32c1c09906ade9d96ebbf56129060
Nour <nour@padev-nalkotob.home>
2019-04-09 23:22:01 +0200

    REST soft

--- uni-rest-api/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/product/ProductWebObject.java
+++ uni-rest-api/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/product/ProductWebObject.java
@@ -18,19 +18,19 @@ import org.nuxeo.uni.ProductAdapter;
 @WebObject(type = "productComputer")
 @Produces(MediaType.TEXT_HTML)
 public class ProductWebObject extends DefaultObject {
-    @GET
-    @Path("{productName}")
-    public String computePrice(@PathParam("productName") String productName) {
-        DocumentModel workflowInstance;
-        	String query = "SELECT * FROM product WHERE ecm:mixinType != 'HiddenInNavigation' AND ecm:isProxy = 0 AND ecm:isVersion = 0 AND ecm:isTrashed = 0 AND productSchema:productName = 'ChocoStick'";
-        	DocumentModelList docs = getContext().getCoreSession().query(query);
-        	
-        	double result = -1;
-        	if(docs.size() == 1) {
-        		HelloService service = Framework.getService(HelloService.class);
-        		ProductAdapter product = docs.get(0).getAdapter(ProductAdapter.class);
-        		result = Framework.getService(HelloService.class).computePrice(product);
-        	}
-        	return String.valueOf(result);
-    }
+	@GET
+	@Path("{productName}")
+	public String computePrice(@PathParam("productName") String productName) {
+		DocumentModel workflowInstance;
+		String query = "SELECT * FROM product WHERE ecm:mixinType != 'HiddenInNavigation' AND ecm:isProxy = 0 AND ecm:isVersion = 0 AND ecm:isTrashed = 0 AND productSchema:productName = '" + productName + "'";
+		DocumentModelList docs = getContext().getCoreSession().query(query);
+
+		double result = -1;
+		if(docs.size() == 1) {
+			HelloService service = Framework.getService(HelloService.class);
+			ProductAdapter product = docs.get(0).getAdapter(ProductAdapter.class);
+			result = Framework.getService(HelloService.class).computePrice(product);
+		}
+		return String.valueOf(result);
+	}
 }
